8b9cd807aa75b1ce8c5dc7a5e4f0fea6b64a4ddd
prayas075 <141097340+prayas075@users.noreply.github.com>
2023-12-24 12:36:19 +0530

    Update StreamQuestions.java

--- src/test2/StreamQuestions.java
+++ src/test2/StreamQuestions.java
@@ -15,7 +15,7 @@ public class StreamQuestions {
 	static List<Employee> employeeList = new ArrayList<Employee>();
 
 	public static void main(String[] args) {
-
+//added from remote
 		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
 		employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
 		employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
